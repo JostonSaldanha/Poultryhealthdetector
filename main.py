2a412beb04d3f697dc8a354aa60569f7ab9545be
JostonSaldanha <jostonsaldanha0@gmail.com>
2024-07-10 01:46:39 +0530

    n

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, JSONResponse
@@ -6,46 +8,41 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
-import os
 
 app = FastAPI()
 
+# Load environment variables from .env file
+load_dotenv()
 
 origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    # allow_methods=["*"] for all methods
     allow_methods=["GET", "POST"],
-    allow_headers=["*"], 
+    allow_headers=["*"],
 )
 
-
-MODEL_PATH='chicken.h5'
-CLASS_NAMES = ["coccidiosis", "healthy", "salmonella"]
-
+# Retrieve environment variables
+MODEL_PATH = os.getenv('MODEL_PATH')
+CLASS_NAMES = os.getenv('CLASS_NAMES').split(",")
 
 MODEL = tf.keras.models.load_model(MODEL_PATH)
-
 
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
     with open("index.html") as f:
         return f.read()
 
-
 def read_file_as_image(data) -> np.ndarray:
     image = Image.open(BytesIO(data)).convert("RGB")
-    image = image.resize((224, 224))  
+    image = image.resize((256, 256))
     return np.array(image)
-
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
@@ -65,7 +62,6 @@ async def predict(file: UploadFile = File(...)):
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
